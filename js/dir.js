4ecd2f6a119843b23612418977414656eff40e01
Linar <zagidullinlinar24@gmail.com>
2019-05-02 18:42:46 +0300

    update directions page

--- js/dir.js
+++ js/dir.js
@@ -17,14 +17,25 @@ var btn1 = document.getElementById("sel-1");
 var btn2 = document.getElementById("sel-2");
 var btn3 = document.getElementById("sel-3");
 
-var active = 0;
+
+
+
+function f1() {
+    $("#aguitar").collapse("toggle");
+}
+
+function f2() {
+    $("#avocal").collapse("toggle");
+}
+
+var active = 1;
 
 btn1.onclick = function () {
-    if (active != 1){
-        if (active == 2){
+    if (active !== 1){
+        if (active === 2){
             btn2.classList.remove("active");
             btn1.classList.add("active");
-
+            f2();
         }
         else {
             btn3.classList.remove("active");
@@ -35,14 +46,16 @@ btn1.onclick = function () {
     else {
         btn1.classList.remove("active");
         active = 0;
+        f1();
     }
 }
 
 btn2.onclick = function () {
-    if (active != 2){
-        if (active == 1){
+    if (active !== 2){
+        if (active === 1){
             btn1.classList.remove("active");
             btn2.classList.add("active");
+            f1();
         }
         else {
             btn3.classList.remove("active");
@@ -53,18 +66,21 @@ btn2.onclick = function () {
     else {
         btn2.classList.remove("active");
         active = 0;
+        f2();
     }
 }
 
 btn3.onclick = function () {
-    if (active != 3){
-        if (active == 2){
+    if (active !== 3){
+        if (active === 2){
             btn2.classList.remove("active");
             btn3.classList.add("active");
+            f2();
         }
         else {
             btn1.classList.remove("active");
             btn3.classList.add("active");
+            f1();
         }
         active = 3;
     }
@@ -73,4 +89,37 @@ btn3.onclick = function () {
         active = 0;
     }
 }
+
+
+// $("#sel-1").click(function () {
+//     if (active != 1){
+//         if (active == 2){
+//             $('#avocal').collapse("hide");
+//         }
+//         else {
+//
+//         }
+//         $('#aguitar').collapse("show");
+//         active = 1;
+//     }
+//     else {
+//         $('#aguitar').collapse("hide");
+//     }
+// })
+//
+// $("#sel-2").click(function () {
+//     if (active != 2){
+//         if (active == 1){
+//             $('#aguitar').collapse("hide");
+//         }
+//         else {
+//
+//         }
+//         $('#avocal').collapse("show");
+//         active = 1;
+//     }
+//     else {
+//         $('#avocal').collapse("hide");
+//     }
+// })
 
